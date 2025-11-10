914b868a30ae9cee40953cbb3910364fb2c20322
Silviu Ojog <sil.ojog@gmail.com>
2025-11-10 10:40:50 +0200

    Tipuri de date

---
+++ 02.tipuri_de_date.py
@@ -0,0 +1,27 @@
+
+## Python este un limbaj loosly typed - o variabila poate lua diferite tipuri
+## Tipuri de date de baza
+
+## INTEGER
+x = 2
+print(x, type(x))  # int
+
+## FLOATING NUMBER
+x = 2.032
+print(x, type(x))  # float
+
+## BOOLEAN
+x = True
+print(x, type(x))  # bool
+
+## STRING
+x = "hello"
+print(x, type(x))  # str
+
+## STRING
+x = 'hello'
+print(x, type(x))  # str
+
+## STRING 
+x = """hello"""  ## folosite la documentatie
+print(x, type(x))  # str
